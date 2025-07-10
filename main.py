51a59ce4ccd89c3e216b2ad8d16ca0df1b6e47a3
Bui Quang Dat <buiquangdat1458@gmail.com>
2025-07-10 14:18:34 +0700

    fix

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ if prompt := st.chat_input("What is up?"):
         stream = client.chat.completions.create(
             model=st.session_state["openai_model"],
             messages=[
-                {"role": "system", "content": "Only respond with icon emojis."},
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
             ],
